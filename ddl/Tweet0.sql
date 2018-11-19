22411dfe68eefd3a7e9262f4769f575cfefa4c30
sinmetal <metal.tie@gmail.com>
2018-11-19 15:48:26 +0900

    add ;

--- ddl/Tweet0.sql
+++ ddl/Tweet0.sql
@@ -2,4 +2,4 @@ CREATE TABLE Tweet0 (
   Id STRING(MAX) NOT NULL,
   CreatedAt TIMESTAMP NOT NULL,
   CommitedAt TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp=true),
-) PRIMARY KEY (Id)
+) PRIMARY KEY (Id);
